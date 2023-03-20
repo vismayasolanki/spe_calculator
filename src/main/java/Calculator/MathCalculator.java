e9a4b5ac485721180bca9debfe572021d5b84e3c
VISMAYA SOLANKI <vismayasolanki@VISMAYAs-MacBook-Pro.local>
2023-03-20 16:21:59 +0530

    First commit

---
+++ src/main/java/Calculator/MathCalculator.java
@@ -0,0 +1,111 @@
+package Calculator;// package src;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MathCalculator {
+    public MathCalculator() {
+    }
+
+    public static void main(String[] args) {
+
+        MathCalculator calculator = new MathCalculator();
+        Scanner input = new Scanner(System.in);
+        double num1, num2;
+        do {
+            System.out.println("Welcome to terminal based Scientific Calculator. \n");
+            System.out.println("Choose the operation");
+            System.out.print("1. Factorial\n" +
+                    "2. Square root\n" +
+                    "3. Power\n" +
+                    "4. Natural Logarithm\n" +
+                    "5. Exit\n" +
+                    "Enter your choice: "
+            );
+            int choice;
+            try {
+                choice = input.nextInt();
+            } catch (InputMismatchException error) {
+                System.out.println("Please Input a integer Choice from the given menu");
+                return;
+            }
+
+            switch (choice) {
+                case 1:
+                    // Factorial
+                    System.out.print("Please input a number whose factorial is to be calculated : ");
+                    double factorial = input.nextDouble();
+                    System.out.println("Factorial of " + factorial + " is : " + calculator.computeFactorial(factorial) + "\n");
+                    break;
+
+                case 2:
+                    // Square root
+                    System.out.print("Enter a number whose square root is to be calculated : ");
+                    double root = input.nextDouble();
+                    System.out.println("Square root of "+ root +" is : " + calculator.computeSquareRoot(root) + "\n");
+                    break;
+
+                case 3:
+                    // Power
+                    System.out.print("Enter the base number : ");
+                    double base = input.nextDouble();
+                    System.out.print("Enter the exponent number : ");
+                    double exponent = input.nextDouble();
+                    System.out.println(base + " raised to power " + exponent + " is : " + calculator.computePower(base, exponent) + "\n");
+                    break;
+
+                case 4:
+                    // Natural log
+                    System.out.print("Enter a number whose log is to be calculated : ");
+                    double log = input.nextDouble();
+                    System.out.println("Natural log of " + log + " is : " + calculator.computeNaturalLog(log) + "\n");
+                    break;
+
+                default:
+                    System.out.println("Exiting Calculator! GoodBye!");
+                    return;
+            }
+        } while (true);
+    }
+
+
+    public static double computeFactorial(double inp) {
+        double result = fact(inp);
+        return result;
+    }
+
+
+    public static double computeSquareRoot(double inp) {
+        double result = Math.sqrt(inp);
+        return result;
+    }
+
+
+    public static double computePower(double inp1, double inp2) {
+        double result = Math.pow(inp1,inp2);
+        return result;
+    }
+
+    public static double computeNaturalLog(double inp) {
+        double result = 0;
+        try {
+
+            if (inp < 0) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            }
+            else {
+                result = Math.log(inp);
+            }
+        } catch (ArithmeticException error) {
+            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+        }
+        return result;
+    }
+    public static double fact(double inp) {
+        double factorial = 1;
+        for(double i = 1; i <= inp; ++i){
+            factorial *= i;
+        }
+        return factorial;
+    }
+}    
---
+++ src/test/java/MathCalculatorTest.java
@@ -0,0 +1,43 @@
+import static org.junit.jupiter.api.Assertions.*;
+import Calculator.MathCalculator;
+
+class MathCalculatorTest {
+
+    MathCalculator calculator = new MathCalculator();
+    private static final double DELTA = 1e-15;
+    @org.junit.jupiter.api.Test
+    void computeFactorial() {
+        assertEquals( 120, MathCalculator.computeFactorial(5), DELTA);
+        assertEquals( 1, MathCalculator.computeFactorial(0), DELTA);
+        assertEquals( 2, MathCalculator.computeFactorial(2), DELTA);
+        assertEquals( 3628800, MathCalculator.computeFactorial(10), DELTA);
+        assertEquals( 720, MathCalculator.computeFactorial(6), DELTA);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void computeSquareRoot() {
+        assertEquals( 4, MathCalculator.computeSquareRoot(16), DELTA);
+        assertEquals( 1, MathCalculator.computeSquareRoot(1), DELTA);
+        assertEquals( 9, MathCalculator.computeSquareRoot(81), DELTA);
+        assertEquals( 6, MathCalculator.computeSquareRoot(36), DELTA);
+    }
+
+    @org.junit.jupiter.api.Test
+    void computePower() {
+        assertEquals(8, MathCalculator.computePower(2, 3), DELTA);
+        assertEquals( 1, MathCalculator.computePower(1, 3), DELTA);
+        assertEquals(81, MathCalculator.computePower(3, 4), DELTA);
+        assertEquals( 64, MathCalculator.computePower(4, 3), DELTA);
+        assertEquals( 64, MathCalculator.computePower(4, 3), DELTA);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void computeNaturalLog() {
+        assertEquals( 0, MathCalculator.computeNaturalLog(1), DELTA);
+
+    }
+
+
+}
